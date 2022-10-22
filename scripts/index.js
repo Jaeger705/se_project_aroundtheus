08fe863ed99bd95c79b467333725a3a2721c21a0
Oiziah <ojoyce01@gmail.com>
2022-10-21 22:47:46 -0400

    java commit 1

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+const initialCards = [
+   {
+    name: 'Yosemite Valley',
+    link: '../images/yosemite-valley.png'
+   },
+   {
+    name: 'Lake Louise',
+    link: '../images/lake-louise.png'
+   },
+   {
+    name: 'Bald Mountains',
+    link: '../images/bald-mountains.png'
+   },
+   {
+    name: 'Latemar',
+    link: '../images/latemar.png'
+   },
+   {
+    name: 'Vanoise National Park',
+    Link: '../images/vanoise-national-park.png'
+   },
+   {
+    name: 'Lago Di Braies',
+    link: '../images/lago-di-braies.png'
+   }
+];
+
+console.log(initialCards);
+
+
+
+
