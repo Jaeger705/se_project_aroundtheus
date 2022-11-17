c3a3602b8a5e750b98fe8fec1ac5866ca9df06c4
Oiziah <ojoyce01@gmail.com>
2022-11-17 08:15:22 -0500

    project 5.12

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,28 @@
 const initialCards = [
    {
-    name: 'Yosemite Valley',
-    link: './images/yosemite-valley.jpg'
-   },
-   {
-    name: 'Lake Louise',
-    link: './images/lake-louise.jpg'
-   },
-   {
-    name: 'Bald Mountains',
-    link: './images/bald-mountains.jpg'
-   },
-   {
-    name: 'Latemar',
-    link: './images/latemar.jpg'
-   },
-   {
-    name: 'Vanoise National Park',
-    link: './images/vanoise-national-park.jpg'
-   },
-   {
-    name: 'Lago Di Braies',
-    link: './images/lago-di-braies.jpg'
-   }
+      name: "Yosemite Valley",
+      link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+    },
+    {
+      name: "Lake Louise",
+      link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+    },
+    {
+      name: "Bald Mountains",
+      link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+    },
+    {
+      name: "Latemar",
+      link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+    },
+    {
+      name: "Vanoise National Park",
+      link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+    },
+    {
+      name: "Lago di Braies",
+      link: "https://code.s3.yandex.net/web-code/lago.jpg",
+    },
 ];
 
 const editButton = document.querySelector('.profile__edit-button');
